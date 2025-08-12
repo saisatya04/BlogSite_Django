b6e214369a72418d7afb7a46481d3bb0ad5a67e3
saisatya04 <saisatya.bv23@gmail.com>
2025-08-12 19:08:52 +0530

    Try writing the production start command without env constants

--- start-django.sh
+++ start-django.sh
@@ -3,7 +3,7 @@ poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
 
 if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn djangocourse.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+    poetry run gunicorn djangocourse.wsgi --workers 2 --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
